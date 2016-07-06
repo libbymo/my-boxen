fed435406cbe07f01f9064bf521c5d26e7c4c95e
libbymo <libbymo@gmail.com>
2016-07-06 10:04:32 -0700

    Updating node, adding postgresql94

--- modules/people/manifests/libbymo/brew.pp
+++ modules/people/manifests/libbymo/brew.pp
@@ -1,10 +1,11 @@
 class people::libbymo::brew {
-	package { 'postgresql' : 
-		provider => 'homebrew',
-		ensure => '9.5.0' }
+	# package { 'postgresql' :
+	# 	provider => 'homebrew',
+	# 	ensure => '9.5.0' }
 	package { 'leiningen' : provider => 'homebrew'}
 	package { 'tree'     : provider => 'homebrew' }
   	package { 'watchman' : provider => 'homebrew' }
   	package { 'httpie' : provider => 'homebrew' }
   	package { 'jq' : provider => 'homebrew' }
+    package { 'postgresql94': provider => 'homebrew'}
 }
